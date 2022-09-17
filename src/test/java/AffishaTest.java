da0b6a1a7b2a0ca4bef99e3e447acc1845e3e865
UliaF80 <uliafedorova297@cmail.com>
2022-09-18 00:47:30 +0300

    add *

--- src/test/java/AffishaTest.java
+++ src/test/java/AffishaTest.java
@@ -2,35 +2,49 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-    public class AffishaTest {
+public class AffishaTest {
 
-        @Test
-        public void test() {
-            Affisha affisha = new Affisha();
+    @Test
+    public void testAffishaAdd() {
+        Affisha affisha = new Affisha();
 
-            affisha.add("Movie 1");
-            affisha.add("Movie 2");
-            affisha.add("Movie 3");
+        affisha.add("Movie 1");
+        affisha.add("Movie 2");
+        affisha.add("Movie 3");
 
-            String[] actual = affisha.findAll();
-            String[] expected = {"Movie 1", "Movie 2", "Movie 3"};
+        String[] actual = affisha.findAll();
+        String[] expected = {"Movie 1", "Movie 2", "Movie 3"};
 
 
-            Assertions.assertArrayEquals(expected, actual);
-        }
-
-        @Test
-        public void testFindlast() {
-            Affisha affisha = new Affisha();
-
-            affisha.add("Movie 1");
-            affisha.add("Movie 2");
-            affisha.add("Movie 3");
-
-            String[] actual = affisha.findLast();
-            String[] expected = {"Movie 3", "Movie 2", "Movie 1"};
-            Assertions.assertArrayEquals(expected, actual);
-        }
-
+        Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+
+    public void testFindAll() {
+        Affisha affisha = new Affisha();
+        affisha.add("Movie 1");
+        affisha.add("Movie 3");
+
+
+        String[] actual = affisha.findAll();
+        String[] expected = {"Movie 1", "Movie 3"};
+    }
+
+    @Test
+    public void testFindlast() {
+        Affisha affisha = new Affisha();
+
+        affisha.add("Movie 1");
+        affisha.add("Movie 2");
+        affisha.add("Movie 3");
+        affisha.add("Movie 4");
+
+        String[] actual = affisha.findLast();
+        String[] expected = {"Movie 4", "Movie 3", "Movie 2", "Movie 1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+}
 
